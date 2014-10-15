077890113b276c6466c71d05fc8b600e76f67e69
crazydoglady <valkharvey@gmail.com>
2014-10-14 20:29:30 -0400

    adding 102 layout info

---
+++ examples/main.js.js
@@ -0,0 +1,10 @@
+var total;
+var firstnumber;
+var secnumber;
+
+
+function add(){
+	firstnumber = document.getElementById('num1').value;
+	secnumber = document.getElementById('num2').value;
+	alert(parseInt(firstnumber) + parseInt(secnumber));
+}
